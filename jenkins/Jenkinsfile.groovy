402c59af4b776b455d0a9a87a8878f86b0007a20
saleh ziad <87468273+Salehziad@users.noreply.github.com>
2023-03-29 14:10:14 +0300

    ad

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -1,71 +1,20 @@
 pipeline {
-    agent any
-      options {
-        ansiColor('xterm')
-    }
+    agent any 
     stages {
-        stage('Build') {
+        stage('Build') { 
             steps {
-                echo("I am in build")
-                sshPublisher(
-                    continueOnError: false, failOnError: true,
-                    publishers: [
-                    sshPublisherDesc(
-                        configName: "dev server",
-                        verbose: true,
-                        transfers: [
-                        sshTransfer(
-                            execCommand: " rm -rf /var/www/api.ciam.agentsoncloud.com_Release_4"
-                        ),
-                        sshTransfer(
-                            sourceFiles: "**/*",
-                            remoteDirectory: "api.ciam.agentsoncloud.com_Release_4",
-                            execCommand:"cd /var/www/api.ciam.agentsoncloud.com_Release_4 && sudo npm i"
-                           
-                        ),
-                    ])
-                ])
+                echo("sadasdasd") 
             }
         }
-        stage('Test') {
+        stage('Test') { 
             steps {
-                echo("I am in Test")
+                echo("sadasdasd") 
             }
         }
-        stage('Deploy') {
+        stage('Deploy') { 
             steps {
-                echo("I am in Deploy")
-                sshPublisher(
-                    continueOnError: false, failOnError: true,
-                    publishers: [
-                    sshPublisherDesc(
-                        configName: "dev server",
-                        verbose: true,
-                        transfers: [
-                         sshTransfer(
-                                 execCommand: "cd /var/www/api.ciam.agentsoncloud.com_Release_4 && pm2 start"
-                         )
-                     
-                    ])
-                ])
+                echo("sadasdasd") 
             }
-        }
-    }
-     post {
-        always {
-           slackSend channel: "#development-jenkins", message: "${env.JOB_NAME}  - #${env.BUILD_NUMBER} : Started . ", color: "good"
-        }
-        success {
-          slackSend channel: "#development-jenkins", message: "${env.JOB_NAME}  - #${env.BUILD_NUMBER} : Build Finshed Success . ", color: "good"
-        }
-        unstable {
-           slackSend channel: "#development-jenkins", message: "${env.JOB_NAME}  - #${env.BUILD_NUMBER} : Build have Error  . ", color: "warning"
-        }
-        failure {
-            slackSend channel: "#development-jenkins", message: "${env.JOB_NAME}  - #${env.BUILD_NUMBER} : Failure Build  . ", color: "danger"
-        }
-        changed {
-             echo 'Things were different before...'
         }
     }
 }
